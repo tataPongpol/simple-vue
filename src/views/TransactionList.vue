a76a02a63b7d366f7893b3370948a24a88adfccf
tataPongpol <pongpol.ro@ku.th>
2021-07-23 03:27:13 +0700

    update project

---
+++ src/views/TransactionList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <transaction-create-form></transaction-create-form>
+  </div>
+</template>
+
+<script>
+import TransactionCreateForm from '@/components/TransactionCreateForm.vue'
+export default {
+  components: {
+    TransactionCreateForm 
+  },
+}
+</script>
+
+<style scoped>
+</style>
